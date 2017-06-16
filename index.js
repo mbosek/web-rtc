729fe559c32704685a21203ba71b2ed90f71d482
mbosek <bosek.mateusz@gmail.com>
2017-06-16 23:30:52 +0200

    Add get user media

---
+++ index.js
@@ -0,0 +1,15 @@
+const errorElement = document.querySelector('#errorMsg');
+const video = document.querySelector('video');
+
+const constraints = window.constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  let videoTracks = stream.getVideoTracks();
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess);
