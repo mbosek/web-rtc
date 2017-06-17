acbaf57a6f4ea76ef5ac8215c1301d5a887c7b80
mbosek <bosek.mateusz@gmail.com>
2017-06-17 14:01:46 +0200

    Draw a frame from the video onto the canvas element

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const video = document.querySelector('video');
+const canvas = window.canvas = document.querySelector('canvas');
+const button = document.querySelector('button');
+
+canvas.width = 480;
+canvas.height = 360;
 
 const constraints = window.constraints = {
   audio: false,
   video: true
+};
+
+button.onclick = () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 };
 
 function handleSuccess(stream) {
