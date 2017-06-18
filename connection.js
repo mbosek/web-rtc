4c0ac71c289dbf2f92e2d9ef98c8831c2afc9c55
mbosek <bosek.mateusz@gmail.com>
2017-06-18 23:00:38 +0200

    Create connection func

---
+++ connection.js
@@ -0,0 +1,26 @@
+function createConnection() {
+  dataChannelSend.placeholder = '';
+  let servers = null;
+  pcConstraint = null;
+  dataConstraint = null;
+
+  window.localConnection = localConnection = new RTCPeerConnection(servers, pcConstraint);
+
+  sendChannel = localConnection.createDataChannel('sendDataChannel', dataConstraint);
+
+  localConnection.onicecandidate = function(e) {
+    onIceCandidate(localConnection, e);
+  };
+  sendChannel.onopen = onSendChannelStateChange;
+  sendChannel.onclose = onSendChannelStateChange;
+
+  window.remoteConnection = remoteConnection = new RTCPeerConnection(servers, pcConstraint);
+
+  remoteConnection.onicecandidate = e => onIceCandidate(remoteConnection, e);
+  remoteConnection.ondatachannel = receiveChannelCallback;
+
+  localConnection.createOffer().then(gotDescription1, onCreateSessionDescriptionError);
+
+  startButton.disabled = true;
+  closeButton.disabled = false;
+}
